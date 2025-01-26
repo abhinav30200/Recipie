f0a6b855de8b7d4be89fd54f348ad740c7686342
abhinav30200 <abhinav2004cs@gmail.com>
2025-01-27 00:52:23 +0530

    in this i have added table and delete recipi option

--- recipies/recipi/views.py
+++ recipies/recipi/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from recipi.models import *
+from django.shortcuts import  redirect
 
 def home(request):
+    querryset = form.objects.all()
+    context = {'recipies': querryset}
     if request.method=="POST":
         data = request.POST
         name = data.get('name')
@@ -13,6 +16,18 @@ def home(request):
             description=description,
             image=image,
         )
-    return render(request,"index.html")
+        querryset=form.objects.all()
+
+        context['recipies'] = querryset
+    return render(request,"index.html",context)
+
+def delete_recipie(request, id):
+    try:
+        recipe = form.objects.get(id=id)
+        recipe.delete()
+    except form.DoesNotExist:
+        pass  # You might want to handle this case with an error message or logging
+    return redirect('home')
+
 
 
--- recipies/recipies/urls.py
+++ recipies/recipies/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from recipi.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('delete_recipie/<id>/',delete_recipie,name='delete_recipie'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=staticfiles_urlpatterns()
